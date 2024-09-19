e903bb32afd7be6467ebe6f4b63a677fb4e0d719
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2024-09-19 20:47:04 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template, jsonify
+import openpyxl
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/upload', methods=['POST'])
+
+
+def upload_file():
+    file = request.files['file']
+    wb = openpyxl.load_workbook(file)
+    sheet = wb.active
+
+    # Assuming the Excel sheet has these fields in the first row
+    data = {
+        'name': sheet['A2'].value,
+        'surname': sheet['B2'].value,
+        'age': sheet['C2'].value
+    }
+
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
